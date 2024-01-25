339c54ce0520236137b76b3e584d9d68080ef2b1
Frederick Stolzle <stolzle@hey.com>
2024-01-25 17:37:57 -0600

    Update package.json and package-lock.json dependencies, and add new post on optimism.

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,98 @@
 import Link from "next/link";
 import { DocumentIcon, ThoughtsIcon } from "./components/icons";
+import { allPosts, Post } from '@/.contentlayer/generated';
+import NextImage, { ImageProps } from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import { format, parseISO } from "date-fns";
+
+const CalloutWithLabel = (props: any) => {
+  return (
+    <div>
+      <div className="bg-stone-700 px-2 text-white w-fit p-1 rounded relative left-7 font-bold top-4  ">
+        {props.title}
+      </div>
+      <div className={'border-2 border-stone-700 text-stone-800 p-3 rounded'}>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+const InfoCalloutWithLabel = (props: any) => {
+  return (
+    <div>
+      <div className="bg-stone-700 px-2 text-white w-fit p-1 rounded relative left-5 font-bold top-4  ">
+        {props.title}
+      </div>
+      <div className={'text-[0.95em] border-2 border-stone-700 text-stone-800 p-3 rounded bg-blue-100'}>
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+const customComponents = {
+  CalloutWithLabel,
+  Image: (props: ImageProps) => <NextImage {...props} />,
+  InfoCalloutWithLabel
+};
+
+export function PostCard(post: Post) {
+  return (
+    <div className="flex flex-col mb-6">
+      <time dateTime={post.updatedOn} className="block text-sm">
+        {format(parseISO(post.updatedOn), 'LLL d, yyyy')}
+      </time>
+      <h2 className='font-[500] underline underline-offset-2 w-fit hover:text-stone-600 hover:decoration-stone-600'>
+        <Link href={post.url}>
+          {post.title}
+        </Link>
+      </h2>
+      <div className='text-sm'>{post.summary}</div>
+    </div>
+  )
+}
+
 
 export default function Home() {
+  // get latest post
+  const latestPost: Post = allPosts.sort((a: Post, b: Post) => new Date(b.publishedOn).getTime() - new Date(a.publishedOn).getTime())[0];
+  // group posts by month, ignoring the latest post
+  const postsByMonth = allPosts.filter((post: Post) => post._raw.flattenedPath !== latestPost._raw.flattenedPath).reduce((acc: any, post: Post) => {
+    const date = new Date(post.publishedOn);
+    const month = date.toLocaleString('default', { month: 'long' });
+    const year = date.getFullYear();
+    const key = `${month} ${year}`;
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(post);
+    return acc;
+  }, {});
+  const Component = useMDXComponent(latestPost.body.code);
   return (
     <section className='flex flex-col text-defaultText'>
-      <h1 className='font-bold text-[2em] mb-4'>The Purpose Of Life Is Joy</h1>
-      <p className="mt-6">
-        Times are delicate. Our culture is miserable and self-hating. Intellectuals and politicians are, at best, desolate and vapid; more often they are destructive.
-        Leaders promote evil ideas and advocate for rights-violating legislation that worsens our lives.
-      </p>
-      <p className="mt-6">
-        Poisoned for over a century by anti-human philosophies, society is becoming more tribal and less individualistic.
-        The state, nation, church, race, and group are important - not the individual.
-        Intellectuals claim that reality is unknowable, that free will is an illusion, and that our minds cannot grasp true knowledge. Ideas like these are devastating.
-      </p>
-      <p className="mt-6">
-        Our culture is group-focused. Instead of praising humanity for its accomplishments and potential, people dwell on mistakes of the past.
-        Some argue that humans are inherently flawed, sinful, and corrupt; that we are inclined to pillage and destroy each other and the planet.
-        Is it any mystery why we are so hateful and angry today? Is it at all surprising why children are unhappy and depressed?
-      </p>
-      <p className="mt-6">
-        Your life is fragile and finite - it's the only one you will ever have. Would you prefer to live in a world where your values are attainable?
-        One in which people were free to pursue happiness and cooperate in peace? A country filled with proud, productive citizens who are grateful they have the opportunity to live?
-      </p>
-      <p className="mt-6">
-        Many go along thinking “Welp, what can I do? This is just the way things are." They've resigned and become complacent.
-      </p>
-      <p className="mt-6">
-        I haven't, and neither should you.
-      </p>
-      <p className="mt-6">
-        <span className='py-[0.15em] px-1 rounded bg-pastelBlue'>Life can be better.</span> But evil ideas are handicapping your life and the lives of those you care about.
-        Bad ideas produce immoral men; immoral men become unprincipled politicians; unprincipled politicians write and pass legislation preventing people and businesses from acting freely. Only reason and persuasion can reverse this trend. Force and power never have and never will make us freer.
-      </p>
-      <p className="mt-6">
-        Ideas determine the course of history. Virtuous convictions gave birth to America. We have abandoned those virtues, and our country is decaying.
-        Children are stressed, anxious, depressed, and drug-addicted precisely because our culture saturates them with defeatist ideas.
-        To effect change, we must reboot and establish a philosophy founded on solid principles.
-      </p>
-      <p className="mt-6">
-        But what are those principles?
-      </p>
-      <ul className="list-disc mt-2">
-        <li className="ms-6 mt-1">a single, objective reality exists</li>
-        <li className="ms-6 mt-1">we use our minds to understand the world</li>
-        <li className="ms-6 mt-1">our minds are capable of grasping reality</li>
-        <li className="ms-6 mt-1">our lives are finite, and we should consider the full range of our lives when deciding what is "good" or "bad"</li>
-        <li className="ms-6 mt-1"><em>man's life</em> is the standard by which we judge ideas and actions</li>
-      </ul>
-      <p className="mt-6">
-        <span className='py-[0.15em] px-1 rounded bg-golden'>The goal of life is to be happy.</span> Despite what politicians, the Pope, and intellectuals may say, you <em>are</em> capable of flourishing in this world.
-        You should be proud of your accomplishments, not feel humiliation for your mistakes. You should be confident in your ability to achieve your goals.
-        You should be ready to judge people based on the content of their mind, on their ideas. You should praise good ideas and condemn evil ones - this is justice.
-      </p>
-      <p className="mt-6">
-        Remember that no matter how hopeless the situation may appear, things can improve. It will take time - but the right ideas will win out in the end. We have reality on our side.
-      </p>
-      <div className="flex flex-wrap mt-4">
+      <article className='prose prose-quoteless prose-md prose-neutral prose-p:text-defaultText prose-ol:text-defaultText prose-ul:text-defaultText prose-blockquote:text-defaultText md:prose-lg mb-8'>
+        <Component components={{ ...customComponents as any }} />
+      </article>
+      {Object.keys(postsByMonth).map((month: string, idx: number) => {
+        return (
+          <div className='mt-8' key={month}>
+            <h2 className='font-bold text-balance text-xl lg:text-3xl'>{month}</h2>
+            <ul className='list-none mt-2'>
+              {postsByMonth[month].map((post: Post) => {
+                return (
+                  <li key={post._raw.flattenedPath} className='ms-6 mt-1'>
+                    <PostCard key={idx} {...post} />
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      }
+      )}
+      {/* <div className="flex flex-wrap mt-4">
         <Link
           href={'/thoughts'}
           as={'/thoughts'}
@@ -72,10 +108,10 @@ export default function Home() {
           title="Longer posts and discussions"
           className='inline-flex items-center gap-x-2 rounded-md bg-white px-3 py-2 ml-6 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
         >
-          Posts
+          Articles
           <DocumentIcon />
         </Link>
-      </div>
+      </div> */}
     </section >
   )
 }
